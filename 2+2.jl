d05d4ed1be69364cb1766c67952d62cf2ccaafb7
Josua Lindemann <josua.lindemann@outlook.de>
2022-06-19 16:06:10 +0200

    Benchmark added and little function to test

--- 2+2.jl
+++ 2+2.jl
@@ -1,7 +1,8 @@
 # Import Packages
 using Pkg  # Package to install new packages
 
-# Install packages 
+
+#= Install packages 
 Pkg.add("DataFrames")
 Pkg.add("CSV")
 Pkg.add("Plots")
@@ -10,8 +11,10 @@ Pkg.add("GLM")
 Pkg.add("StatsPlots")
 Pkg.add("MLBase")
 Pkg.add("Flux")
+Pkg.add("BenchmarkTools")
+=#
 
-# Load the installed packages
+#= Load the installed packages
 using DataFrames
 using CSV
 using Plots
@@ -20,7 +23,28 @@ using GLM
 using Statistics
 using StatsPlots
 using MLBase
+=#
 using Flux
+using BenchmarkTools
 
 # Enable printing of 1000 columns
-ENV["COLUMNS"] = 1000
+#ENV["COLUMNS"] = 1000
+
+
+function testsum(n)
+    x = Float64[2 5 8]
+    y = Float64[5 2 6]
+
+    if n <= 3 && n >= 1
+        k = 0
+        for i in 1:n
+            k = sum(x .- y)
+            x[i] = k
+            y[i] = -k
+        end
+    end
+    
+    return x, y
+end
+
+@btime testsum(3)
