83bd564f03a119097e9ebdb4a37523bf0151307d
Josua Lindemann <josua.lindemann@outlook.de>
2022-06-23 15:03:54 +0200

    added functions

--- 2+2.jl
+++ 2+2.jl
@@ -141,3 +141,15 @@ for i in 1:10
     end
 end
 
+
+# functions
+function f1!(x)
+    x[1] = 9999
+    return(x)
+end
+
+ia = Int64[0,1,2]
+println("Array ia: ", ia)
+
+f1!(ia)
+println("Argument passing by reference: ", ia)
